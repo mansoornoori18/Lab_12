dc92c7a3f97f62184f320a32c531b7729e01afd5
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-09-19 22:50:29 -0700

    initial

---
+++ Lab12.cpp
@@ -0,0 +1,16 @@
+#include <iostream>  // COMSC-210 | Lab 12 | Mansoor Noori
+#include <array>     // IDE Visual Studio,
+#include <fstream>
+
+using namespace std;
+
+int main(){
+
+ 
+
+    cout << " hello " << endl;
+
+
+
+    return 0;
+}
