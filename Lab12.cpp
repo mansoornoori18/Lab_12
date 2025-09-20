73dec67078617750d05014d6c18266fe4dbd7d89
mansoornoori18 <mnoori926@insite.4cd.edu>
2025-09-19 23:02:50 -0700

    update

--- Lab12.cpp
+++ Lab12.cpp
@@ -1,10 +1,19 @@
 #include <iostream>  // COMSC-210 | Lab 12 | Mansoor Noori
 #include <array>     // IDE Visual Studio,
-#include <fstream>
+#include <fstream>   // for file input&output
 
+// to vaoid using std through out the project
 using namespace std;
 
 int main(){
+
+    // create an array to hold the stock quantity 
+    array<int, 30> stock{};
+
+    // open the file that contain the stock numbers 
+    ifstream file("inventory.txt");
+
+
 
  
 
